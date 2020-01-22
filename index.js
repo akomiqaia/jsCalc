f2fad7a0847a0923dba3da5edd483dd72e3027e3
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-01-22 13:19:36 +0000

    updated read me file

---
+++ index.js
@@ -0,0 +1,8 @@
+let currentNumber = document.querySelector(".current-number").innerHTML
+
+// all the operators and numbers typed by the user
+let wholeFormula = document.querySelector(".whole-formula").innerHTML
+
+
+
+console.log(currentNumber, wholeFormula)
