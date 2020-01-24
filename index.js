c52aa924f42bd2bf4917de481db6f205dfaedd46
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-01-24 13:24:21 +0000

    finished diplay number function

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-let currentNumber = document.querySelector(".current-number").innerHTML
+let currentNumber = document.querySelector(".current-number")
+let wholeFormula = document.querySelector(".whole-formula")
+let numbers = document.querySelectorAll(".number")
+let operators = document.querySelectorAll(".operators")
 
-// all the operators and numbers typed by the user
-let wholeFormula = document.querySelector(".whole-formula").innerHTML
+// functions needed 
+  // clear function to delete the typed number
+        // double click to start all over again
+  // function displayNumber()
+
+  numbers.forEach(number => {
+    number.addEventListener('click', () => {
+      if (number.innerText === "." && currentNumber.innerText.toString().includes(".")) return
+      currentNumber.innerText += number.innerText
+    })
+  })
+  // function displayWholeFormula()
+
+  // function operation
+      // if operator is pressed the current number display has to clear out
+      
+      operators.forEach(operator => {
+        operator.addEventListener('click', () => {
+          console.log(operator.innerText)
+        })
+      })
+  // funtion equals()
 
 
 
-console.log(currentNumber, wholeFormula)
+
+console.log(currentNumber)
