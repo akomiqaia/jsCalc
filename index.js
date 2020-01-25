206836da20ee1b8392027b45a9915e9b492af910
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-01-25 17:50:30 +0000

    equal button functionality added, also managing operators at basic level

--- index.js
+++ index.js
@@ -2,31 +2,62 @@ let currentNumber = document.querySelector(".current-number")
 let wholeFormula = document.querySelector(".whole-formula")
 let numbers = document.querySelectorAll(".number")
 let operators = document.querySelectorAll(".operators")
+let clear = document.getElementById('clear')
+let equals = document.getElementById('equals')
+// clear function to delete the typed number
+      // double click to start all over again
+clear.addEventListener('click', () => {
+  currentNumber.innerText = ""
+} )
 
-// functions needed 
-  // clear function to delete the typed number
-        // double click to start all over again
-  // function displayNumber()
+clear.addEventListener('dblclick', () => {
+  wholeFormula.innerText = ""
+})
 
-  numbers.forEach(number => {
-    number.addEventListener('click', () => {
-      if (number.innerText === "." && currentNumber.innerText.toString().includes(".")) return
-      currentNumber.innerText += number.innerText
-    })
+// function displayNumber()
+numbers.forEach(number => {
+  number.addEventListener('click', () => {
+
+    // don't not allow a number to begin with multiple zeros.
+    if(currentNumber.innerText.startsWith("0") && number.innerText === "0") {
+      currentNumber.innerText = "0"
+      return
+    }
+
+    // if equal pressed and user is trying to yype a new number clear everything
+    if (wholeFormula.innerText.toString().includes("=")) {
+      wholeFormula.innerText = ""
+      currentNumber.innerText = ""
+    }
+
+    // When the decimal element is clicked, a dot should append to the currently displayed value;
+    //  two dots in one number should not be accepted.
+    if (number.innerText === "." && currentNumber.innerText.toString().includes(".")) return
+
+    currentNumber.innerText += number.innerText
   })
-  // function displayWholeFormula()
+})
 
-  // function operation
-      // if operator is pressed the current number display has to clear out
-      
-      operators.forEach(operator => {
-        operator.addEventListener('click', () => {
-          console.log(operator.innerText)
-        })
-      })
-  // funtion equals()
+// function displayWholeFormula()
+operators.forEach(operator => {
+  operator.addEventListener('click', () => {
+    
+
+    if (wholeFormula.innerText.toString().includes("=")) {
+      wholeFormula.innerText = ` ${currentNumber.innerText} ${operator.innerText}`
+      currentNumber.innerText = ""
+      return
+    }
+    wholeFormula.innerText += ` ${currentNumber.innerText} ${operator.innerText}`
+    currentNumber.innerText = ""
+  })
+})
+// funtion equals()
+equals.addEventListener('click', () => {
+  wholeFormula.innerText += currentNumber.innerText
+  currentNumber.innerText = eval(wholeFormula.innerText).toFixed(4)
+  wholeFormula.innerText += ` ${equals.innerText} ${currentNumber.innerText}`
+})
 
 
 
-
-console.log(currentNumber)
